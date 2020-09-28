bb4c2e2880dad78cd35844d70b5fc08fda7f5b86
TheIdiotTourist <theidiottourist@gmail.com>
2020-09-28 16:40:27 -0400

    navbar routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/index'
-  get 'pages/resume'
-  get 'pages/graphicdesign'
-  get 'pages/coding'
-  get 'pages/film'
-  get 'pages/music'
-  get 'pages/writing'
+  get 'pages/resume', as: :resume
+  get 'pages/graphicdesign', as: :graphicdesign
+  get 'pages/coding', as: :coding
+  get 'pages/film', as: :film
+  get 'pages/music', as: :music
+  get 'pages/writing', as: :writing
   root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
